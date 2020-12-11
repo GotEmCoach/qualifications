6fe6781d9f8958f5c237400c687097f806092b14
gotemcoach <mclarkfyrue@gmail.com>
2020-12-11 14:25:54 -0500

    In the beginning

---
+++ main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <argp.h>
+
+//TO DO: declaration of C data types
+//TO DO: create/implement function with different arrays
+//TO DO: handle command line args
+//TO DO: math skillz
+//TO DO: file stuff
+//TO DO: write functions that do all the things
+//TO DO: input validation
+//TO DO: pointers (int)
+//TO DO: flow control
+//TO DO: networking stuff
+//TO DO: hash function
+//TO DO: sort routine
+//TO DO: state machine
+//TO DO: Dijksta's Algo weighted graph
+//TO DO: use bit wise operator
+//TO DO: memory control
+//TO DO: Thread control
+
+int main()
---
+++ main.py
@@ -0,0 +1,10 @@
+#/usr/bin/env python3
+
+#TO DO: Argparser
+#TO DO: Declare all the stuff.
+#TO DO: Math Skillz
+#TO DO: File stuff
+#TO DO: Function stuff
+#TO DO: Data validation, error handling
+#TO DO: Flow control
+#TO DO: Network stuff
